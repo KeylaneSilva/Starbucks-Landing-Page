7a22871aef8141b64565094752af4079a4cec7a3
Keylane <keylanessilva07@gmail.com>
2021-11-07 00:32:25 -0300

    add função de trocar img e cor

---
+++ index.js
@@ -0,0 +1,8 @@
+function imgSlider(imagens){
+    document.querySelector('.starbucks').src = imagens
+}
+
+function trocaCor(cor){
+    const circle = document.querySelector('.circle')
+    circle.style.background = cor
+}
